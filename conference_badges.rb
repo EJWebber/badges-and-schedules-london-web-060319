ff9f07e820ea440b2f18fbbf4a6626713dd56c14
Ed Webber <edwebber06@gmail.com>
2019-05-22 09:35:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,10 @@
 # Write your code here.
+speakers=["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(speakers)
   array.each(|x| puts badge_maker(x))
 end
 end
