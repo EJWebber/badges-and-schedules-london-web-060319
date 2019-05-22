a8cf61cd0022ec1b885950cbd6e605573ec588a4
Ed Webber <edwebber06@gmail.com>
2019-05-22 09:48:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index do |speaker, index|
+  speakers.each_with_index.map do |speaker, index|
     "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
 end
 end
